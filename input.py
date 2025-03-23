13674e9639b27e3152f86cef6acc6cc3f80c2843
Dzahabi84 <dzahabi2007@gmail.com>
2025-03-24 02:22:02 +0700

    add input.py

---
+++ input.py
@@ -0,0 +1,4 @@
+nama = input("Masukkan nama anda... : ")
+umur = input("Masukkan umur anda... : ")
+greeting_sentence = "Halo " + nama + " Berusia " + umur + " Selamat Datang Di hexalab.tech"
+print(greeting_sentence)
--- variable.py
+++ variable.py
@@ -1,2 +1,17 @@
+# Variable Python
+# Tidak ada konsep "Hoisting" jadinya variable dibaca dari atas sampai bawah (Hierarki)
+
 price = 1000
-print(price)
+print("starting price :", price)
+
+discount = 0.5
+print("discount :", discount)
+
+price = price * discount
+print("after discount price :", price)
+
+product_name = "Mouse Gaming"
+print("product name :", product_name)
+
+is_discount = True
+print("is discount? ", is_discount)
