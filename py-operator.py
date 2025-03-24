c6c2bf682b48bea83193b3e7bab216bef69ca6bf
Dzahabi84 <dzahabi2007@gmail.com>
2025-03-25 01:52:34 +0700

    add py-operator.py

---
+++ py-operator.py
@@ -0,0 +1,15 @@
+# Comparison Operator
+print(5>=5)
+print(5 > 10)
+print(5<=5)
+print(5 < 10)
+print(6==2)
+print(6!=2)
+
+print("-----------")
+
+# Logic Operator
+print(True and False)
+print(True and True)
+print(True or False)
+print(False or False)
