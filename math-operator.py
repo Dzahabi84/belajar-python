ad11cc6c9fd8f6bd3fc584bce14ca866f5a8252d
Dzahabi84 <dzahabi2007@gmail.com>
2025-03-25 01:17:10 +0700

    add math-operator.py

---
+++ math-operator.py
@@ -0,0 +1,14 @@
+x = 10
+y = 3
+
+print(x + y) #Penjumlahan
+print(x - y) #Pengurangan
+print(x * y) #Perkalian
+print(x ** y) #Perpangkatan
+print(x / y) #Pembagian
+print(x // y) #Pembagian Pembulatan
+print(x % y) #Sisa Pembagian (Modulo)
+x += 2
+print(x)
+y += 3
+print(y)
