318fc26a76f64a18c7788f393fcc4bc918edaf1f
Dzahabi84 <dzahabi2007@gmail.com>
2025-03-25 01:10:03 +0700

    add type-string.py

---
+++ type-string.py
@@ -0,0 +1,19 @@
+name = 'Aqila Riza Dzah"Abi"'
+print(name)
+print(name[2])
+print(name[-2])
+print(name[10:19])
+
+# Formatted String
+age = 31
+message = f"Umur kamu adalah {age}"
+print(message)
+
+# String Method
+naration = "Jakarta Adalah Ibu Kota Indonesia Terbaik"
+print(naration.upper())
+print(naration.lower())
+print(naration.capitalize())
+print(naration.replace("Terbaik", "Ter macet"))
+print("Jakarta" in naration)
+print("Depok" in naration)
