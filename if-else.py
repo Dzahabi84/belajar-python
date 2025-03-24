998545bde3a44e5a67b7c3c8a83558a86cc15c18
Dzahabi84 <dzahabi2007@gmail.com>
2025-03-25 01:37:15 +0700

    add if-else.py

---
+++ if-else.py
@@ -0,0 +1,11 @@
+is_day = False
+is_night = False
+
+if is_day:
+    print("Selamat Siang")
+elif is_night:
+    print("Selamat Malam")
+elif is_day & is_night:
+    print("Tinggal Dimana Lu")
+else:
+    print("Anjir gapunya waktu")
