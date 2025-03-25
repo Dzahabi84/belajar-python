4726710b49f9fcfbf56607ce0d7354cf8db6b123
Dzahabi84 <dzahabi2007@gmail.com>
2025-03-26 02:41:36 +0700

    add for in in py-looping.py

--- py-looping.py
+++ py-looping.py
@@ -1,3 +1,15 @@
+# For In
+name = "Agung"
+list_makanan = ["Apel", "Jeruk", "Mangga"]
+for item in name:
+    print(item)
+
+for item in range(1,10,2):
+    print(item)
+
+for item in list_makanan:
+    print(item)
+
 # While
 
 message = input("Masukkan pesan : ")
