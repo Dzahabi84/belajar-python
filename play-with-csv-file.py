601d8cf35de98cfa3074cef66924690468b921d7
Dzahabi84 <dzahabi2007@gmail.com>
2025-03-28 02:56:40 +0700

    modified play-with-txt-file.py n play-with-csv-file.py with with-block

--- play-with-csv-file.py
+++ play-with-csv-file.py
@@ -1,7 +1,9 @@
+# Ketika pake open wajib di .close atau pakai With Block
 import csv
 
-users = open("users.csv", "r")
-users_csv = csv.reader(users, delimiter=",")
+with open("users.csv", "r") as users:
+    users = open("users.csv", "r")
+    users_csv = csv.reader(users, delimiter=",")
 
-for row in users_csv:
-    print(f"{row[0]}, yang memiliki username {row[1]} dengan role nya sebagai {row[2]}")
+    for row in users_csv:
+        print(f"{row[0]}, yang memiliki username {row[1]} dengan role nya sebagai {row[2]}")
