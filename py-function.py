1479cb3f85c16a318d81be81040c39aa496611b5
Dzahabi84 <dzahabi2007@gmail.com>
2025-03-28 02:18:15 +0700

    add py-function.py

---
+++ py-function.py
@@ -0,0 +1,12 @@
+# Normal Function
+def halo_kamu(name, age):
+    print(f"Hi, kamu {name} berusia {age} apa kabar!")
+
+halo_kamu("Abi", 17)
+
+# With Argument
+def balance(income, outcome):
+    return income - outcome
+
+aqila_balance = balance(outcome=3000, income=5000)
+print(aqila_balance)
